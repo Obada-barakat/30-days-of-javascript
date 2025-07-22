5cb5db4ec224547fd968208ae3241d33c8cbf6a0
Obada barakat <obada.baracat1@gmail.com>
2025-07-22 13:05:36 +0400

    Generate QR Code

---
+++ day-27-generate-QR-code/main.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#qrValue');
+const generateBtn = document.querySelector('#generate')
+const imgBox = document.querySelector('#imgBox');
+const qrImage = document.querySelector('#qrImage');
+
+function generateQR() {
+    if (input.value.length > 0) {
+        qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example' +
+        input.value;
+        imgBox.classList.add('show');
+    } else {
+        input.classList.add('error');
+        setTimeout(() => {
+            input.classList.remove('error')
+        }, 1000)
+    }
+}
+
+generateBtn.addEventListener('click', generateQR)
